ee35ce1118272cdb7bb77324d5966146dedcc4af
danid <danid@.com>
2025-12-16 16:42:20 +0100

    ejercicos  TS

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,51 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angu');
+  protected readonly title = signal('Daniel DAuria');
+
+  ngOnInit(){
+    //EJERCICIO 1
+    const nombre:string = 'Daniel';
+    const edad: number = 19;
+    const activo : boolean = true;
+
+    //EJERCICIO 2
+    function calcularAreaRectangulo( a:number,b:number):number{
+      return a*b;
+    }
+    console.log(calcularAreaRectangulo(5,10));
+
+    //EJERCICO 3
+    interface Persona{
+        nombre: string
+        edad: number
+        correo: string
+    }
+
+    let usuario :Persona={nombre: 'alex',edad:18,correo:'alexfonta@gmail.com'};
+    console.log(usuario);
+    let id: string | number = '29';
+
+    //EJERCICIO 4
+    function analizarId(id: string | number):string{
+        if(typeof id === 'string'){
+            return 'Es de tipo string.';
+        }else{
+            return 'Es de tipo number';
+        }
+    }
+
+    console.log(analizarId(id));
+
+    //EJERCICIO 5
+    const numeros: number[] = [40, 1, 5, 200, 3];
+
+    function ordenar(numeros:number[]):number[]{
+        numeros.sort((a, b) => a - b);
+        return numeros;
+    }
+
+    console.log(ordenar(numeros))
+
+  }
 }
